739b76378d770a0545d6cb0b3ed023c81a3d4c56
Dmitry Bashkatov <dbashkatov@gmail.com>
2021-06-07 12:34:13 +0300

    working skeleton

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿#region Prelude
+using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace Skeleton
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+using VRageMath;
+using VRage.Game;
+using VRage.Collections;
+using Sandbox.ModAPI.Ingame;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using Sandbox.Game.EntityComponents;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using VRage.Game.ObjectBuilders.Definitions;
+
+// Change this namespace for each script you create.
+namespace Skeleton {
+    public sealed class Program : MyGridProgram {
+    // Your code goes between the next #endregion and #region
+#endregion
+
+
+public Program() {
+}
+
+
+public void Main(string args) {
+}
+
+
+#region PreludeFooter
     }
 }
+#endregion
